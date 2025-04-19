e332176abbaa4b503694169e3a0dc26528ac7b27
Icecream <joshuakleinmeulman@gmail.com>
2025-04-19 23:04:51 +1000

    Initial Commit

---
+++ src/main/java/net/icecream/slaycream/SlayCream.java
@@ -0,0 +1,14 @@
+package net.icecream.slaycream;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SlayCream implements ModInitializer {
+	public static final String MOD_ID = "slaycream";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/icecream/slaycream/SlayCreamClient.java
@@ -0,0 +1,10 @@
+package net.icecream.slaycream;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SlayCreamClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
